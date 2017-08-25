b33877e9b008a06bce824ecf44107a26f3a9a30c
Zane Hitchcox <zwhitchcox@gmail.com>
2017-08-25 01:05:02 -0400

    update

--- init.sh
+++ init.sh
@@ -5,7 +5,9 @@ sudo apt-get -y install tmux git vim
 git config --global user.name "Zane Hitchcox"
 git config --global user.email zwhitchcox@gmail.com
 git config --global core.editor vim
-ssh-keygen -t rsa -b 4096 -C "zwhitchcox@gmail.com" -f ~/.ssh/id_rsa -N ""
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -t rsa -b 4096 -C "zwhitchcox@gmail.com" -f ~/.ssh/id_rsa -N ""
+fi
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 echo;
