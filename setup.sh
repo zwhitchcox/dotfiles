42fa46adb62d73baca412895bee2e0c95116d038
Zane Hitchcox <zwhitchcox@gmail.com>
2017-09-08 09:34:38 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 #!/usr/local/bin bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 nvm install node
+sudo ln -s $(which npm) /usr/bin/npm
+sudo ln -s $(which node) /usr/bin/node
 find . -maxdepth 1 -regextype posix-egrep -regex "\.\/\..*" ! -name .git -exec cp -t .. {} +
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +PlugInstall +qa
-if [ "$HOSTTYPE" = "x86_64" ]
-  then
-    echo $'syntax enable\nset background=light\ncolorscheme solarized' >> ~/.vimrc
-fi
