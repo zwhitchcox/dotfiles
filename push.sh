5bbe4f1032679da2feef2d14037b9e67b39a7423
Zane Hitchcox <zwhitchcox@gmail.com>
2018-04-24 13:10:05 -0400

    update

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp -r ~/.bashrc .
-cp -r ~/.vimrc .
-cp -r ~/.tmux.conf .
-cp -r ~/.gitconfig .
-cp -r ~/.snippets .
-cp -r ~/.aliases .
-cp -r ~/bin .
-git add . -A && git commit -m 'update' && git push
