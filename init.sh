f7489aeac3b137b51137af43a717f64cfad16a32
Zane Hitchcox <zwhitchcox@gmail.com>
2018-07-06 12:45:47 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sleep 1
 
-echo "What is your full name?"
-read name
-echo "What is your full email?"
-read email
+if [ -n "$name" ];
+then echo "What is your full name?" && read name;
+else echo "What is your full email?" && read email;
+fi
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
@@ -12,11 +12,7 @@ echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sud
 sudo apt-get update
 sudo apt-get -y install tmux git cmake apt-transport-https curl ca-certificates software-properties-common google-chrome-stable xclip
 
-xdg-mime default google-chrome-stable.desktop text/html
-xdg-mime default google-chrome-stable.desktop x-scheme-handler/http
-xdg-mime default google-chrome-stable.desktop x-scheme-handler/https
-xdg-mime default google-chrome-stable.desktop x-scheme-handler/about
-
+xdg-settings set default-web-browser google-chrome-stable.desktop
 
 git config --global user.name "$name"
 git config --global user.email "$email"
